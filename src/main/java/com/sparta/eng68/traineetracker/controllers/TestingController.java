6b30111fc41b56094ac55967c3065d8992d0b258
Timothy Oluwole <TOluwole@spartaglobal.com>
2020-10-20 14:33:46 +0100

    changed weekly report page to change using param 'id'  to choose trainee. Latest report has edit option before its deadline. Trainees without reports yet are shown a message

--- src/main/java/com/sparta/eng68/traineetracker/controllers/TestingController.java
+++ src/main/java/com/sparta/eng68/traineetracker/controllers/TestingController.java
@@ -1,37 +1,15 @@
 package com.sparta.eng68.traineetracker.controllers;
 
-import com.sparta.eng68.traineetracker.entities.WeekReport;
-import com.sparta.eng68.traineetracker.services.WeekReportService;
 import com.sparta.eng68.traineetracker.utilities.Pages;
 import com.sparta.eng68.traineetracker.utilities.Role;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class TestingController {
-    WeekReportService weekReportService;
-
-    @Autowired
-    public TestingController(WeekReportService weekReportService) {
-        this.weekReportService = weekReportService;
-    }
 
     @GetMapping("/home")
     public String getHomePage() {
         return Pages.accessPage(Role.ANY, Pages.HOME_PAGE);
-    }
-
-    @GetMapping("/guide")
-    public String getTraineeGuide() { return Pages.TRAINEE_GUIDE;}
-
-    @GetMapping("/weeklyReports")
-    public String getTraineeGuide(Model model) {
-        List<WeekReport> reports = weekReportService.getAllReports();
-        model.addAttribute("reports", reports);
-        return Pages.TRAINEE_REPORT;
     }
 }

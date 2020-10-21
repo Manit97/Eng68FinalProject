009edc73a088ec34564a6ec90bca5f76435d63a4
ConorMalone14 <CMalone@spartaglobal.com>
2020-10-21 10:59:33 +0100

    added recover password page

--- src/main/java/com/sparta/eng68/traineetracker/services/CustomUserDetailsService.java
+++ src/main/java/com/sparta/eng68/traineetracker/services/CustomUserDetailsService.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -20,7 +22,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(s);
+        Optional<User> userOptional = userRepository.findByUsername(s);
+        User user = userOptional.get();
 
         if (user == null) {
             throw new UsernameNotFoundException(s);

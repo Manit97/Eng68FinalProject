36e9e7e60ee7c90f8fde3cfe3496a1f653e7775e
Timothy Oluwole <TOluwole@spartaglobal.com>
2020-10-21 15:51:46 +0100

    retrieval as a trainer of week reports by week and by trainee, can edit using button, changed sidebar to show options depending on who has signed in

--- src/main/java/com/sparta/eng68/traineetracker/controllers/TrainerHomeController.java
+++ src/main/java/com/sparta/eng68/traineetracker/controllers/TrainerHomeController.java
@@ -1,13 +1,25 @@
 package com.sparta.eng68.traineetracker.controllers;
 
+import com.sparta.eng68.traineetracker.services.TrainerService;
 import com.sparta.eng68.traineetracker.utilities.Pages;
 import com.sparta.eng68.traineetracker.utilities.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TrainerHomeController {
+
+    private final TrainerService trainerService;
+
+    @Autowired
+    public TrainerHomeController(TrainerService trainerService) {
+        this.trainerService = trainerService;
+    }
 
     @GetMapping("/trainerHome")
     public String getTrainerHome() {
